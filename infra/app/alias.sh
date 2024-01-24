488ee5259262b48f190959cfce8cf55e0431da28
yuki-kai <issi0430bjc@gmail.com>
2024-01-25 00:52:15 +0900

    first commit

---
+++ infra/app/alias.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# basic
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ll='ls -l'
+alias lla='ls -la'
+alias ..='cd ..'
+alias ...='cd ../..'
+# laravel
+alias art='php artisan'
+alias arts='art serve'
+alias artr='art route:list'
+alias artmc='art make:controller'
+alias artmm='art make:model'
+alias artmr='art make:request'
+alias artms='art make:seeder'
+alias migrate='art migrate'
+alias seed='art db:seed'
+alias rollback='art migrate:rollback'
+alias fresh='art migrate:fresh'
+alias cc='php artisan config:clear && php artisan cache:clear && php artisan view:clear'
+alias cca='php artisan config:clear && php artisan cache:clear && php artisan view:clear && php artisan config:cache'
+alias test='./vendor/bin/phpunit --testdox'
+# git
+alias gb='git branch'
+alias gc='git checkout'
+alias gcd='gc develop'
+alias gcs='gc stage'
+alias gcm='gc main'
+alias gs='git status'
+alias gp='git pull'
