7df0a9d5edaf9bd97dc02913c8fc1de541973676
yuki-kai <issi0430bjc@gmail.com>
2024-02-04 02:14:29 +0900

    404ページの作成

--- resources/ts/App.tsx
+++ resources/ts/App.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { Login } from './Pages/Auth/Login';
 import { Dashboard } from './Pages/Dashboard';
+import { NotFound } from './Pages/Error/NotFound';
 
 export const App = () => {
     return (
         <Routes>
             <Route path='/login'  element={<Login />} />
             <Route path='/dashboard'  element={<Dashboard />} />
-            <Route path="*" element={<>404</>} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 };
---
+++ resources/ts/Pages/Error/NotFound.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export const NotFound = () => {
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-4">
+                    <div className="card">
+                        <div className="card-header">404 NOT FOUND</div>
+                        <div className="card-body">お探しのページが見つかりませんでした</div>
+                        <Link className="btn btn-primary" to="/login">戻る</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
