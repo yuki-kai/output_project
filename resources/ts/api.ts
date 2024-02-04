82c539ec55aa63b809b2246488cb7d75c6fc67b0
yuki-kai <issi0430bjc@gmail.com>
2024-02-04 18:43:07 +0900

    環境変数の設定

--- resources/ts/api.ts
+++ resources/ts/api.ts
@@ -1,17 +1,18 @@
 import axios from 'axios';
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+axios.defaults.baseURL = import.meta.env.VITE_APP_URL;
 
 export const login = async (email: string, password: string): Promise<void> => {
-    await axios.post("http://localhost/api/login",{ email, password });
+    await axios.post("/api/login",{ email, password });
     return;
 };
 
 export const checkAuthenticated = async (): Promise<{ checked: boolean, isAuthenticated:boolean }> => {
-    const response = await axios.get("http://localhost/api/checkAuthenticated");
+    const response = await axios.get("/api/checkAuthenticated");
     return response.data;
 };
 
 export const logout = async (): Promise<void> => {
-    await axios.post("http://localhost/api/logout")
+    await axios.post("/api/logout")
     return;
 };
