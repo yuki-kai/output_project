60d948598eabf92c59379feaf5caf6ab7b1e70a4
yuki-kai <issi0430bjc@gmail.com>
2024-02-11 00:23:23 +0900

    ローディング画面を作成

---
+++ resources/ts/Components/Loading.tsx
@@ -0,0 +1,15 @@
+import ReactLoading from 'react-loading';
+
+export const Loading = () => {
+    return (
+        <div className="container mt-5">
+            <div className="d-flex align-items-center justify-content-center">
+                <ReactLoading
+                    color="grey"
+                    type="spin"
+                    className="mt-5"
+                />
+            </div>
+        </div>
+    );
+};
